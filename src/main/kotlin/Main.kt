2f0db5b9b7b8065c912a82c16ec3228efc9d3263
rizvan <rizvangaripov@gmail.com>
2023-11-28 12:38:06 +0300

    add cycle

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -10,6 +10,7 @@ fun main() {
     val f = 1000
 
     var nearestResult = Int.MAX_VALUE
+    var resultList = listOf<List<Int>>()
 
     val possibleAs = mutableListOf<Int>()
     for (i in 1..f / a) {
@@ -47,21 +48,18 @@ fun main() {
         population.add(listOf(possibleAs[i], possibleBs[i], possibleCs[i], possibleDs[i], possibleEs[i]))
     }
 
-    val descendants = mutableListOf<List<Int>>()
+    var descendants = mutableListOf<List<Int>>()
     for (i in 0..<population.size - 1 step 2) {
         descendants.add(listOf(possibleAs[i], possibleBs[i], possibleCs[i], possibleDs[i + 1], possibleEs[i + 1]))
         descendants.add(listOf(possibleAs[i + 1], possibleBs[i + 1], possibleCs[i + 1], possibleDs[i], possibleEs[i]))
-
-        a * possibleAs[i] + b + possibleBs[i] + c * possibleCs[i] + d * possibleDs[i + 1] + e * possibleEs[i + 1]
-        a * possibleAs[i + 1] + b * possibleBs[i + 1] + c * possibleCs[i + 1] + d * possibleDs[i] + e * possibleEs[i]
     }
 
-    val probabilities = descendants.map { element ->
+    var probabilities = descendants.map { element ->
         val value = a * element[0] + b * element[1] + c * element[2] + d * element[3] + e * element[4]
         1 / (1 + abs(f - value).toDouble())
     }.toDoubleArray()
 
-    val selectedIndices = mutableSetOf<Int>()
+    var selectedIndices = mutableSetOf<Int>()
     val numSelectedElements = 10
 
     repeat(numSelectedElements) {
@@ -69,16 +67,50 @@ fun main() {
         selectedIndices.add(selectedIndex)
     }
 
-    val selectedDescendants = selectedIndices.map { descendants[it] }
+    var selectedDescendants = selectedIndices.map { descendants[it] }
 
-    var isChange = false
     for (descendant in selectedDescendants) {
         val value = a * descendant[0] + b * descendant[1] + c * descendant[2] + d * descendant[3] + e * descendant[4]
         if (abs(f - value) < nearestResult) {
-            isChange = true
-            nearestResult = value
+            nearestResult = abs(f - value)
+            resultList = listOf(descendant)
         }
     }
+
+    var isChange = true
+    while (isChange) {
+        descendants = mutableListOf()
+        for (i in 0..<selectedDescendants.size - 1 step 2) {
+            descendants.add(listOf(selectedDescendants[i][0], selectedDescendants[i][1], selectedDescendants[i][2], selectedDescendants[i + 1][3], selectedDescendants[i + 1][4]))
+            descendants.add(listOf(selectedDescendants[i + 1][0], selectedDescendants[i + 1][1], selectedDescendants[i + 1][2], selectedDescendants[i][3], selectedDescendants[i][4]))
+        }
+
+        probabilities = descendants.map { element ->
+            val value = a * element[0] + b * element[1] + c * element[2] + d * element[3] + e * element[4]
+            1 / (1 + abs(f - value).toDouble())
+        }.toDoubleArray()
+
+        selectedIndices = mutableSetOf()
+
+        repeat(numSelectedElements) {
+            val selectedIndex = rouletteWheelSelection(probabilities)
+            selectedIndices.add(selectedIndex)
+        }
+
+        selectedDescendants = selectedIndices.map { descendants[it] }
+
+        isChange = false
+        for (descendant in selectedDescendants) {
+            val value = a * descendant[0] + b * descendant[1] + c * descendant[2] + d * descendant[3] + e * descendant[4]
+            if (abs(f - value) < nearestResult) {
+                isChange = true
+                nearestResult = abs(f - value)
+                resultList = listOf(descendant)
+            }
+        }
+    }
+
+    println(resultList)
 }
 
 fun rouletteWheelSelection(weights: DoubleArray): Int {
